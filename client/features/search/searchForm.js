cc42eb9a3fb3e1c98bfa82f432976e7aaceb3dfc
Alexandra Bunty <buntyalexandra@gmail.com>
2023-03-09 12:14:49 -0500

    search form data is set up as local state. Form captures user input correctly

--- client/features/search/searchForm.js
+++ client/features/search/searchForm.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 // function for matching a cocktail to ingredients
 /*
@@ -10,18 +10,65 @@ example:
 
 psuedocode
 Gather the form inputs and put them in a array (an array of strings)
-Loop through the cocktailData array, inspecting the ingredients array at each object
+need a place to store matching cocktails
+Loop through the cocktailData array, inspecting the ingredients array at each object 
+    - if the first element in the array matches any of the elements in the user inputs array
+        - move onto the next element and do the check
+            - if we end up matching all of the cocktail ingredients, we store the cocktail object in our matching cocktails array
+    - if the first element does not match and of the user inputs
+        - move onto the next cocktail
 */
 
+// set up each of the inputs as state
+// do an on change that updates the input with the input from the event
+
 const SearchForm = () => {
+  const [ingredient1, setIngredient1] = useState("");
+  const [ingredient2, setIngredient2] = useState("");
+  const [ingredient3, setIngredient3] = useState("");
+  const [ingredient4, setIngredient4] = useState("");
+  const [ingredient5, setIngredient5] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(
+      ingredient1,
+      ingredient2,
+      ingredient3,
+      ingredient4,
+      ingredient5
+    );
+    document.getElementById("searchForm").reset();
+  };
+
   return (
     <div>
-      <form>
-        <input type="text" name="ingredient1" required></input>
-        <input type="text" name="ingredient2" required></input>
-        <input type="text" name="ingredient3" required></input>
-        <input type="text" name="ingredient4"></input>
-        <input type="text" name="ingredient5"></input>
+      <form id="searchForm" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          onChange={(e) => setIngredient1(e.target.value)}
+          required
+        ></input>
+        <input
+          type="text"
+          onChange={(e) => setIngredient2(e.target.value)}
+          required
+        ></input>
+        <input
+          type="text"
+          onChange={(e) => setIngredient3(e.target.value)}
+          required
+        ></input>
+        <input
+          type="text"
+          onChange={(e) => setIngredient4(e.target.value)}
+          required
+        ></input>
+        <input
+          type="text"
+          onChange={(e) => setIngredient5(e.target.value)}
+          required
+        ></input>
         <button type="submit">Can I Cocktail?</button>
       </form>
     </div>
